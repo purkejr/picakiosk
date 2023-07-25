ef38fa1c589ea9cacef353a05fb839474b3de8d4
purkejr <edinpuric5@gmail.com>
2023-07-25 12:13:19 +0200

    222 fix

--- setup.sh
+++ setup.sh
@@ -1,38 +1,27 @@
 #!/bin/bash
 
-# Install necessary packages
+# Update packages
 sudo apt-get update
-sudo apt-get install -y chromium-browser xdotool x11-utils unclutter python3-tk
-command -v git >/dev/null 2>&1 || { echo >&2 "Installing Git..."; sudo apt-get install -y git; }
 
-# Install AnyDesk
-wget -qO - https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
+# Install necessary packages
+sudo apt-get install -y python3-pip chromium-browser xdotool
+
+# Install Tkinter for Python
+pip3 install python-tk
+
+# Install Anydesk
+wget -qO- https://keys.anydesk.com/repos/DEB-GPG-KEY | sudo apt-key add -
 echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
-sudo apt update
-sudo apt install anydesk
+sudo apt-get update
+sudo apt-get install anydesk
 
-# Create the kiosk.sh file
-cat << EOF > /home/spica/kiosk.sh
-#!/bin/bash
-while true; do
-    if ! pgrep chromium-browser > /dev/null; then
-        /usr/bin/chromium-browser --noerrdialogs --kiosk http://timespace.spica.com
-    fi
-    sleep 1s
-done
-EOF
-
-# Make the kiosk.sh file executable
-chmod +x /home/spica/kiosk.sh
-
-# Create the kiosk_gui.py file
-cat << EOF > /home/spica/kiosk_gui.py
+# Save Python script to a file
+cat > kiosk.py <<EOF
 import tkinter as tk
 from tkinter import messagebox
 import os
 import getpass
 
-# Define your password here
 PASSWORD = "spica"
 
 def check_password():
@@ -51,17 +40,15 @@ def save_changes():
     new_url = url_entry.get()
     kiosk_file_path = "/home/spica/kiosk.sh"
 
-    # Read all lines of the file into a list
     with open(kiosk_file_path, 'r') as file:
         lines = file.readlines()
 
-    # Find the line that starts with "/usr/bin/chromium-browser" and change the URL
     for i in range(len(lines)):
-        if lines[i].startswith("/usr/bin/chromium-browser"):
-            lines[i] = "/usr/bin/chromium-browser --noerrdialogs --kiosk " + new_url + "\\n"
+        if lines[i].lstrip().startswith("/usr/bin/chromium-browser"):
+            leading_spaces = len(lines[i]) - len(lines[i].lstrip())
+            lines[i] = " " * leading_spaces + "/usr/bin/chromium-browser --noerrdialogs --kiosk " + new_url + "\n"
             break
 
-    # Write the modified lines back to the file
     with open(kiosk_file_path, 'w') as file:
         file.writelines(lines)
 
@@ -84,5 +71,19 @@ save_button = tk.Button(root, text="Shrani spremembe", command=save_changes)
 root.mainloop()
 EOF
 
-# Make the kiosk_gui.py file executable
-chmod +x /home/spica/kiosk_gui.py
+# Save Bash script to a file
+cat > kiosk.sh <<EOF
+#!/bin/bash
+while true; do
+    if ! pgrep chromium-browser > /dev/null; then
+        /usr/bin/chromium-browser --noerrdialogs --kiosk https://google.com
+    fi
+    sleep 1s
+done
+EOF
+
+# Make Bash script executable
+chmod +x kiosk.sh
+
+# Run Python script
+python3 kiosk.py &
